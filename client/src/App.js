92125f2772a9ba55ca6ff408f98a4b3ceac2fdfb
preetanam123 <80145584+preetanam123@users.noreply.github.com>
2023-03-04 18:04:39 +0530

    cloudinary

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import { BrowserRouter as Router  , Routes, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/Login/SignUp";
+import Upload from "./Upload";
+
 
 function App() {
   return (
   <Router>
     <Routes>
-      <Route path="/" element={<Navbar />} />
-      <Route path="login" element={<Login />} />
-      <Route path="Signup" element={<SignUp/>} />
+      <Route path="/" element={<Upload />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp/>} />
     </Routes>
   </Router>
   );
---
+++ client/src/Upload.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+import React from "react";
+
+const Upload = () => {
+  const [image, setImage] = useState("");
+  const [url, setUrl] = useState("");
+  const handleUpload = () => {
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "clash-of-codes");
+    data.append("cloud_name", "dkjknjdfs");
+    fetch("https://api.cloudinary.com/v1_1/dkjknjdfs/image/upload", {
+      method: "POST",
+      body: data,
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setUrl(data.url);
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div>
+      <div>
+        <input
+          type="file"
+          onChange={(e) => setImage(e.target.files[0])}
+        ></input>
+        <button onClick={handleUpload}>Upload</button>
+      </div>
+      <div>
+        <h1>Uploaded image will be displayed here</h1>
+        <img src={url} />
+      </div>
+    </div>
+  );
+};
+
+export default Upload;
