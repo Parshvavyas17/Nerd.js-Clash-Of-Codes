e419789926f2d094ff82ffbaf5225ec21b4ca88f
Salonishah06 <salonishah306@gmail.com>
2023-03-04 20:24:18 +0530

    Upload half done

--- client/src/Upload.js
+++ client/src/Upload.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const Upload = () => {
   const [image, setImage] = useState("");
@@ -22,17 +22,35 @@ const Upload = () => {
 
   };
   return (
-    <div>
-      <div>
-        <input
-          type="file"
-          onChange={(e) => setImage(e.target.files[0])}
-        ></input>
-        <button onClick={handleUpload}>Upload</button>
+    <div className="bg-[#E4D9FF]">
+      <Navbar />
+      <div >
+        <p className="text-4xl text-black font-bold text-center p-10 ">
+          Let the World Recognize You
+        </p>
+        <div className="text-center">
+          <div class="relative">
+            <input type="file" class="absolute opacity-0 " onChange={(e) => setImage(e.target.files[0])} />
+            <div class="border-4 border-dotted border-gray-700 p-10 mx-64 mt-10">
+              <button class="px-16 py-4 text-white font-bold text-lg bg-[#4051A3] rounded-lg ">
+                Upload Image
+              </button>
+            </div>
+            <div className="h-10 ">
+
+            </div>
+
+
+          </div>
+
+        </div>
+        {/* <button onClick={handleUpload} className="">Upload</button>
+        <h1>Uploaded image will be displayed here</h1> */}
+        {/* <img src={url} /> */}
       </div>
+
+
       <div>
-        {/* <h1>Uploaded image will be displayed here</h1>
-        <img src={url} /> */}
       </div>
     </div>
   );
