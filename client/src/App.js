a6d3970212f611a679a51e5d29764065e928a15f
preetanam123 <80145584+preetanam123@users.noreply.github.com>
2023-03-04 14:13:35 +0530

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router  , Routes, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
   <Router>
     <Routes>
       <Route path="/" element={<Navbar />} />
+      <Route path="login" element={<Login />} />
     </Routes>
   </Router>
   );
---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,27 @@
+import React from "react";
+import bg from "../../assets/bg.png";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <>
+      <div className="w-full h-fit bg-cover bg-gradient-to-r from-sky-500 to-indigo-500 min-h-screen min-w-screen">
+        <h1 className="text-6xl font-bold text-white px-10 pt-10">Name</h1>
+        <br />
+        <h6 className="text-lg font-semibold text-white px-10">A platform for collaboration and progress</h6>
+        <div className="px-16 py-16 bg-white mx-16 mt-6 w-1/3 h-[100%] rounded-lg" >
+          hdbcrvb
+          uyvbetrb
+          buevbtiu
+          ervbt
+          eurbvtuv
+          bhebrt
+          eurbvtuv
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
