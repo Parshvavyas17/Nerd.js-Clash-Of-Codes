e4584ff58448b4037196f1b2fdc4d811606f803e
Salonishah06 <salonishah306@gmail.com>
2023-03-04 22:41:04 +0530

    Upload page

--- client/src/Upload.js
+++ client/src/Upload.js
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import React from "react";
 import Navbar from "./components/Navbar";
+import { MdVerifiedUser } from "react-icons/md";
+import { TbCloudUpload, TbFaceId } from "react-icons/tb";
+import { BsDatabaseAdd } from "react-icons/bs";
+
+
+
 
 const Upload = () => {
   const [image, setImage] = useState("");
@@ -29,31 +35,74 @@ const Upload = () => {
           Let the World Recognize You
         </p>
         <div className="text-center">
+
+
           <div class="relative">
-            <input type="file" class="absolute opacity-0 " onChange={(e) => setImage(e.target.files[0])} />
-            <div class="border-4 border-dotted border-gray-700 p-10 mx-64 mt-10">
-              <button class="px-16 py-4 text-white font-bold text-lg bg-[#4051A3] rounded-lg ">
-                Upload Image
-              </button>
+            <div class="border-4 border-dotted border-gray-800 p-10 mx-64 mt-10">
+              <div class="relative">
+                <input type="file" class="absolute opacity-0" onChange={(e) => setImage(e.target.files[0])} />
+                <button class="px-16 py-4 text-white font-bold text-lg bg-[#4051A3] rounded-lg ">
+                  Upload File
+                </button>
+              </div>
             </div>
-            <div className="h-10 ">
+              <hr className="border border-gray-500 mt-20" />
+
+              <div className=" flex mt-10">
+                <div className="w-1/4">
+                  <MdVerifiedUser className="flex ml-44 cursor-pointer " size="70" />
+                  <div className="text-black font-bold text-lg">
+                    Secure your Documents
+                  </div>
+                </div>
+                <div className="w-1/4">
+                  <TbCloudUpload className="flex ml-44 cursor-pointer" size="70" />
+                  <div className="text-black font-bold text-lg">
+                    Upload Your Documents in seconds
+                  </div>
+                </div>
+                <div className="w-1/4">
+                  <TbFaceId className="flex ml-44 cursor-pointer " size="70" />
+                  <div className="text-black font-bold text-lg">
+                    Identify your face
+                  </div>
+                </div>
+                <div className="w-1/4">
+                  <BsDatabaseAdd className="flex ml-44 cursor-pointer " size="70" />
+                  <div className="text-black font-bold text-lg">
+                    Keep A record of your data
+                  </div>
+                </div>
+
+
+
+              </div>
+              <hr className="border border-gray-500 mt-10" />
+
+
+            
+            <div>
+
+              <footer class="bg-[#090E40] py-4 mt-24">
+                <div class="container mx-auto text-center text-white font-bold h-10">
+                  <p>Our Website And the services we provide</p>
+
+                </div>
+              </footer>
+
+
+
+
 
             </div>
-
-
           </div>
-
         </div>
-        {/* <button onClick={handleUpload} className="">Upload</button>
-        <h1>Uploaded image will be displayed here</h1> */}
-        {/* <img src={url} /> */}
-      </div>
 
 
-      <div>
+        <div>
+        </div>
       </div>
-    </div>
-  );
+    </div>  );
 };
 
-export default Upload;
+      export default Upload;
