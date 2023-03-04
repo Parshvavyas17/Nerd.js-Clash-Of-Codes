84f5ac0bfab2247d188bf1784109b857d1bafbaa
RichaRokadiya <richarajeshrokadiya@gmail.com>
2023-03-04 16:39:04 +0530

    Login

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,23 +1,32 @@
 import React from "react";
 import bg from "../../assets/bg.png";
 import "./Login.css";
+import login from "../../assets/login.jpeg"
+import google from "../../assets/google.jpg"
 
 const Login = () => {
   return (
     <>
-      <div className="w-full h-fit bg-cover bg-gradient-to-r from-sky-500 to-indigo-500 min-h-screen min-w-screen">
-        <h1 className="text-6xl font-bold text-white px-10 pt-10">Name</h1>
+      <div className="w-full h-fit bg-cover bg-[#090E40] min-h-screen min-w-screen">
+        <h1 className="text-6xl font-bold text-white px-16 pt-10">Name</h1>
         <br />
-        <h6 className="text-lg font-semibold text-white px-10">A platform for collaboration and progress</h6>
-        <div className="px-16 py-16 bg-white mx-16 mt-6 w-1/3 h-[100%] rounded-lg" >
-          hdbcrvb
-          uyvbetrb
-          buevbtiu
-          ervbt
-          eurbvtuv
-          bhebrt
-          eurbvtuv
-
+        <h6 className="text-xl font-semibold text-white px-16">A platform for collaboration and progress</h6>
+        <div className="px-16 py-16 bg-white mx-40 mt-6 mb-6 h-[100%] rounded-3xl flex justify-center" >
+          <div className="w-1/2 h-4/5">
+            <div className="font-bold text-4xl text-center pb-6">Login <br/>
+            </div>
+            <div className="flex justify-center align-middle">
+              <button className="flex justify-evenly items-center mr-6 text-center w-96 bg-[#D9D9D9] border-2 border-[#090E40] rounded-full py-1 font-semibold text-lg cursor-pointer">
+                <img src={google} className="h-3/4 rounded-full" />Login with Google
+              </button>
+            </div>
+            <div className="justify font-semibold text-[#090E40] text-xl px-16 pt-2">
+              Email
+            </div>
+          </div>
+          <div className="w-1/2 h-4/5">
+            <img src={login} className="h-6/7 w-4/5"/>
+          </div>
         </div>
       </div>
     </>
