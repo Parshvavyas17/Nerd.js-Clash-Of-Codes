9efbd69daf37d636739da456156e502cb5ad482f
Parshvavyas17 <parshvavyas07@gmail.com>
2023-03-04 22:59:13 +0530

    Request query for images

--- ml/app.py
+++ ml/app.py
@@ -6,13 +6,30 @@ app = Flask(__name__)
 CORS(app)
 
 
+# @app.route('/api/img/detect', methods=['POST'])
+# def detect():
+#     url = request.form['url']
+#     string = main.getDetectedImage(url)
+#     if string == "":
+#         return "No Face Detected :("
+#     return send_file(string, mimetype='image/png')
+
 @app.route('/api/img/detect', methods=['POST'])
 def detect():
-    url = request.form['url']
+    url = request.args['url']
     string = main.getDetectedImage(url)
     if string == "":
         return "No Face Detected :("
     return send_file(string, mimetype='image/png')
+
+# @app.route('/api/img/detect', methods=['POST'])
+# def detect():
+#     jsonData = request.get_json()
+#     url = jsonData['url']
+#     string = main.getDetectedImage(url)
+#     if string == "":
+#         return "No Face Detected :("
+#     return send_file(string, mimetype='image/png')
 
 
 if __name__ == '__main__':
