d562a76d13a8e405d352dec4cde689d467062dd4
Parshvavyas17 <parshvavyas07@gmail.com>
2023-03-05 11:52:56 +0530

    Fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
-import { BrowserRouter as Router  , Routes, Route} from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
-import SignUp from "./pages/Login/SignUp";  
+import SignUp from "./pages/Login/SignUp";
 import Landing from "./pages/Login/Landing";
 import Face from "./uploads/Face";
 import Gender from "./uploads/Gender";
@@ -11,18 +10,18 @@ import Docs from "./pages/Login/Docs";
 
 function App() {
   return (
-  <Router>
-    <Routes>  
-      <Route path="/" element={<Landing />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/face" element={<Face />} />
-      <Route path="/gender" element={<Gender />} />
-      <Route path="/verify" element={<Verify />} />
-      <Route path="/age" element={<Age />} />
-      <Route path="/docs" element={<Docs />} />
-    </Routes>
-  </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/face" element={<Face />} />
+        <Route path="/gender" element={<Gender />} />
+        <Route path="/verify" element={<Verify />} />
+        <Route path="/age" element={<Age />} />
+        <Route path="/docs" element={<Docs />} />
+      </Routes>
+    </Router>
   );
 }
 
