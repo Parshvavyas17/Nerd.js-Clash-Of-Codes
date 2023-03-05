a6093f7646f22ac31827bb1cdb8b8b720b3435fe
preetanam123 <80145584+preetanam123@users.noreply.github.com>
2023-03-05 10:38:48 +0530

    gfgjw

--- client/src/uploads/Face.js
+++ client/src/uploads/Face.js
@@ -7,6 +7,30 @@ import { BsDatabaseAdd } from "react-icons/bs";
 
 const Face = () => {
   const [image, setImage] = useState("");
+  const [url, setUrl] = useState("");
+
+  const handleUrlUpload = (e) => {
+    e.preventDefault();
+    fetch("http://127.0.0.1:8000/api/img/detect", {
+          method: "POST",
+          // mode: "no-cors",
+          // cache: "no-cache",
+          headers: {
+            "Content-Type": "application/json",
+            // 'Accept': 'application/json'
+          },
+          body: JSON.stringify({ url }),
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then((res) => {
+            console.log(res);
+            if (res.url === "No Face Detected :(") alert("No face detected");
+            else window.location.href = res.url;
+          })
+          .catch((error) => console.log(error));
+  }
 
   const handleUpload = (e) => {
     e.preventDefault();
@@ -47,6 +71,10 @@ const Face = () => {
       .catch((err) => console.log(err));
   };
 
+  const handleUrl = (e) => {
+    setUrl(e.target.value);
+  };
+
   return (
     <div className="bg-[#E4D9FF]">
       <Navbar isLoggedIn />
@@ -61,6 +89,7 @@ const Face = () => {
               <div className="flex">  
                 <br />
                 <form>
+                  <input type="text" onChange={handleUrl} name="url" placeholder="Enter the URL of the image to be processed"/>
                   {" "}
                   <input
                     type="file"
@@ -72,7 +101,7 @@ const Face = () => {
               </div>
 
               <button
-                onClick={handleUpload}
+                onClick={(image) ? handleUpload : (url) ? handleUrlUpload : () => alert("Pls upload image or enter image url")}
                 className="px-16 py-4 text-white font-bold text-lg bg-[#4051A3] rounded-lg "
               >
                 Upload Image
