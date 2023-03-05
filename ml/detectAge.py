3eb140b9e7292874e994ffb626031f92ab8629e3
preetanam123 <80145584+preetanam123@users.noreply.github.com>
2023-03-05 10:16:28 +0530

    age

--- ml/detectAge.py
+++ ml/detectAge.py
@@ -1,4 +1,19 @@
+from deepface import DeepFace
+import cv2
+import matplotlib.pyplot as plt
 
 
-def predictAge(url):
-    return
+# img = cv2.imread('jennifer.jfif')
+# plt.imshow(img)
+
+obj = DeepFace.analyze(img_path = "sloni.png", actions = [ 'age'])
+
+# if(obj[0]["dominant_gender"] == "Man"):
+#     print("Male")
+# elif(obj[0]["dominant_gender"] == "Woman"):
+#     print("Female")
+
+print(obj[0]['age'])
+
+
+
