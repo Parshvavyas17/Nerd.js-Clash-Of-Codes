3bafd2a2e36a2b2f88c308afd5dd827bfdf5c9ab
preetanam123 <80145584+preetanam123@users.noreply.github.com>
2023-03-05 08:40:47 +0530

    model gender

---
+++ ml/testgender.py
@@ -0,0 +1,19 @@
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+
+
+model = load_model('model.h5')
+
+test_image = image.load_img('richa.png', target_size=(64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
+#C:\Users\preet\OneDrive\Desktop\pyhton\ronaldo1.jfif
+result = model.predict(test_image)
+
+if result[0][0] == 1:
+    prediction = 'female'
+else:
+    prediction = 'male'
+
+print(prediction)
